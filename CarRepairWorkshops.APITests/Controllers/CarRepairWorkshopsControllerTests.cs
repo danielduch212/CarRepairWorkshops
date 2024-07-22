43f18571dad09cd7d433c61a174540f6c8bed4f8
danielduch212 <danielduchnowicz1@gmail.com>
2024-07-22 16:40:18 +0200

    Added authenticated controller tests

---
+++ CarRepairWorkshops.APITests/Controllers/CarRepairWorkshopsControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using CarRepairWorkshops.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using FluentAssertions;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization.Policy;
+using CarRepairWorkshops.APITests;
+
+namespace CarRepairWorkshops.API.Controllers.Tests
+{
+    public class CarRepairWorkshopsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public CarRepairWorkshopsControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory.WithWebHostBuilder(builder=>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                });
+                
+            });
+
+        }
+
+        [Fact()]
+        public async Task GetAll_ForValidRequest_Returns200Ok()
+        {
+
+            
+            //arrange
+            var client = _factory.CreateClient();
+
+            //act
+            var result = await client.GetAsync("/api/carRepairWorkshops?pageNumber=1&pageSize=5");
+
+            //assert
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        }
+
+        [Fact()]
+        public async Task GetAll_ForBadRequest_Returns400()
+        {
+            //arrange
+            var client = _factory.CreateClient();
+
+            //act
+            var result = await client.GetAsync("/api/carRepairWorkshops");
+
+            //assert
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+
+        }
+
+        [Fact()]
+        public void GetAllWorkshopsAsyncTest()
+        {
+
+        }
+
+        [Fact()]
+        public void CreateWorkshopTest()
+        {
+
+        }
+
+        [Fact()]
+        public void DeleteWorkshopTest()
+        {
+
+        }
+    }
+}
---
+++ CarRepairWorkshops.APITests/Middlewares/ErrorHandlingMiddlewareTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using CarRepairWorkshops.Domain.Exceptions;
+using CarRepairWorkshops.Domain.Entities;
+
+namespace CarRepairWorkshops.API.Middlewares.Tests;
+
+public class ErrorHandlingMiddlewareTests
+{
+    [Fact()]
+    public async void InvokeAsync_WhenNoExceptionThrown_ShouldCallNextDelegate()
+    {
+        var logger = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleware = new ErrorHandlingMiddleware(logger.Object);
+        var context = new DefaultHttpContext();
+        var nextDelegateMock = new Mock<RequestDelegate>();
+
+        //act
+
+        await middleware.InvokeAsync(context, nextDelegateMock.Object);
+
+        //assert
+
+        nextDelegateMock.Verify(next => next.Invoke(context), Times.Once);
+    }
+
+    [Fact()]
+    public async void InvokeAsync_WhenNotFoundExceptionThrown_ShouldSetStatusCode404()
+    {
+        var logger = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleware = new ErrorHandlingMiddleware(logger.Object);
+        var context = new DefaultHttpContext();
+        var notFoundException = new NotFoundException(nameof(CarRepairWorkshop), "1");
+
+
+        //act
+        await middleware.InvokeAsync(context, _ => throw notFoundException);
+
+        //assert
+        context.Response.StatusCode.Should().Be(404);
+    }
+}
